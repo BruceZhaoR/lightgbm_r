d1fa4a1af58884f980a517085dd9c390b5902469
BruceZhao <brucezhaor2016@gmail.com>
2019-01-14 16:32:36 +0800

    Update install-pkgs.sh

--- pkgdown/install-pkgs.sh
+++ pkgdown/install-pkgs.sh
@@ -1,5 +1,5 @@
 Rscript -e 'install.packages(c("devtools","pkgdown"), repos = "https://cran.rstudio.com", type="both")'
-Rscript -e 'devtools::build(pkg = ".", vignettes = FALSE, args = c("--no-multiarch"))'
+Rscript -e 'devtools::build(pkg = ".", vignettes = FALSE, args = c("--keep-empty-dirs"))'
 
 # Install the package
 Rscript -e 'version <- gsub("Version: ","",grep("Version: ",readLines(con = file.path(".", "DESCRIPTION")),value = TRUE));tarball <- file.path(getwd(),"..", sprintf("lightgbm_%s.tar.gz", version));system(sprintf("R CMD INSTALL %s --no-multiarch", tarball))'
