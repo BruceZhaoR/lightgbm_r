5268b3aa9c7fc9895c9f59aec4a4a218562c66ef
BruceZhao <brucezhaor2016@gmail.com>
2019-01-14 11:22:07 +0800

    Update and rename build-site.sh to build-site.R

---
+++ pkgdown/build-site.R
@@ -0,0 +1,19 @@
+setwd(".")
+devtools::document()
+
+#options(pkgdown.internet=FALSE)
+library(pkgdown)
+
+clean_site()
+init_site()
+build_home(quiet = FALSE,preview = FALSE)
+build_reference(preview = FALSE)
+# # to-do
+# build_articles(preview = FALSE)
+# build_tutorials(preview = FALSE)
+# build_news(preview = FALSE)
+
+# # don't work
+# pkgdown::build_site(pkg = ".", examples = FALSE, document = TRUE,
+#                     run_dont_run = TRUE, seed = 1014, lazy = FALSE,
+#                     override = list(), preview = NA, new_process = FALSE)
--- pkgdown/build-site.sh
+++
@@ -1,1 +0,0 @@
-Rscript -e "devtools::document();options(pkgdown.internet=FALSE);pkgdown::build_site()"
